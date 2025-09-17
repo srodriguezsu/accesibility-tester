4af667280ade43d7ef6523d779020c80d540c47c
Sebastian Rodriguez Suarez <129565458+srodriguezsu@users.noreply.github.com>
2025-09-17 15:07:06 -0500

    path in env

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.post("/api/test", async (req, res) => {
     let browser;
     try {
         const browser = await puppeteer.launch({
+            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
             args: ["--no-sandbox", "--disable-setuid-sandbox"]
         });
 
